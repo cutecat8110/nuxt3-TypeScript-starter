75cd5b4a42d5e593b7957c8d8a79b13ed336ef20
DESKTOP-ENRHNU6\User <cutecat8110@gmail.com>
2023-05-30 09:44:24 +0800

    chore: Tailwind CSS + Prettier plugins

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    container: {
+      padding: '1rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px'
+      },
+      center: true
+    },
+    screens: {
+      '2xl': { max: '1535px' },
+      // => @media (max-width: 1535px) { ... }
+      xl: { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+      lg: { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+      md: { max: '767px' },
+      // => @media (max-width: 767px) { ... }
+      sm: { max: '639px' }
+      // => @media (max-width: 639px) { ... }
+    }
+  }
+}
